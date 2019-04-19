9327e9f9dd9ae8e4ad2f03e0a6215797a2800c8d
Laney McGlohon <lmcglohon@gmail.com>
2019-04-19 12:13:10 -0700

    Update methods

--- common/db/migrations/120_clean_slugs_for_repositories.rb
+++ common/db/migrations/120_clean_slugs_for_repositories.rb
@@ -1,13 +1,31 @@
 require_relative 'utils'
-require '../backend/app/lib/slugs/slug_helpers_generate'
 
 Sequel.migration do
   up do
     $stderr.puts("Cleaning slugs for repositories")
 
     self[:repository].all.each do |r|
-      slug = clean_slug(r[:repo_code])
+      # This code is duplicated from SlugHelpers#clean_slug
+      slug = r[:repo_code].downcase
+                          .gsub(" ", "_")
+                          .gsub("--", "")
+                          .gsub("'", "")
+                          .gsub(/[&;?$<>#%{}|\\^~\[\]`\/@=:+,!.]/, "")
+                          .gsub(/_[_]+/, "_")
+                          .gsub(/^_/, "")
+                          .gsub(/_$/, "")
+                          .slice(0, 50)
+
+      if slug.match(/^(\d)+$/)
+        slug = slug.prepend("__")
+      end
       self[:repository].where(:id => r[:id]).update(:slug => slug)
     end
+
+    # Repo slugs should default to auto generation
+    alter_table(:repository) do
+      set_column_default :is_slug_auto, 1
+    end
+
   end
 end
